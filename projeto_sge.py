f8893a9c2ff723f30bc3b235be023507222592f6
thallesborges <thallesborg3s@gmail.com>
2024-11-18 12:20:40 -0300

    começando o menu

--- projeto_sge.py
+++ projeto_sge.py
@@ -6,3 +6,26 @@ def limpar_terminal():
 	else:
 		os.system("clear")
 
+
+
+estoque = []
+
+print("Sistema de Gerenciamento de Estoque\n1. Cadastrar Produto")
+
+comando = int(input("Opção do Menu: "))
+
+match comando:
+	case 1:
+		limpar_terminal()
+		produto = {}
+		produto["nome"] = input('Nome: ')
+		produto["categoria"] = input('Categoria: ')
+		produto["quantidade"] = int(input('Quantidade: '))
+		produto["preco"] = float(input('Valor: '))
+
+		estoque.append(produto)
+
+
+print(estoque)
+
+
